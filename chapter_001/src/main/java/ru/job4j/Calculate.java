2037dc38b146e3034fa28c054b272c9fd874541b
alexandervmokhov <alexander.v.mokhov@gmail.com>
2018-03-10 22:24:01 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Class Calculate для выполнения задания 1.1. Создание Hello World [#141]
+* @author Alexander Mokhov (alexander.v.mokhov@gmail.com)
+* @version 1
+* @since 10.03.2018
+*/
 public class Calculate {
+	
+	/**
+	* Main.
+	* @param args - args.
+	*/	
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
