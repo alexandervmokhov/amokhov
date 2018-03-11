8eb13c9ae9fa52aafc88ac9bc6c47964fc67d1b7
alexandervmokhov <alexander.v.mokhov@gmail.com>
2018-03-12 01:34:58 +0300

    Обновление файлов

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Alexander Mokhov (alexander.v.mokhov@gmail.com)
-* @version $Id$
-* @since 10.03.2018
-*/
+ * Test.
+ *
+ * @author Alexander Mokhov (alexander.v.mokhov@gmail.com)
+ * @version $Id$
+ * @since 10.03.2018
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Alexander Mokhov";
-    String expect = "Echo, echo, echo : Alexander Mokhov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Alexander Mokhov";
+        String expect = "Echo, echo, echo : Alexander Mokhov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
