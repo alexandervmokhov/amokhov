1f8469a72f4e3d234265a0f61a56cf501c7b8676
alexandervmokhov <alexander.v.mokhov@gmail.com>
2018-05-18 19:41:22 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,36 +1,48 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * @author Alexander Mokhov (alexander.v.mokhov@gmail.com)
  * @version $Id$
- * @since 16.05.18
+ * @since 18.05.18
  */
 public class PaintTest {
 
     /**
-     * Алгоритм работы теста:
-     *
-     * Получаем ссылку на стандартный вывод в консоль.
-     * Создаем буфер для хранения вывода.
-     * Заменяем стандартный вывод на вывод в пямять для тестирования.
-     * Выполняем действия пишущие в консоль.
-     * Проверяем результат вычисления
-     * Возвращаем обратно стандартный вывод в консоль.
+     * Поле содержит дефолтный вывод в консоль.
      */
+    private final PrintStream stdout = System.out;
+
+    /**
+     * Буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                new String(this.out.toByteArray()),
                 is(
                         new StringBuilder()
                                 .append("+ + + +")
@@ -41,6 +53,23 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
+                                .add("  ^  ")
+                                .add(" ^ ^ ")
+                                .add("^^^^^")
+                                .add("")
+                                .toString()
+                )
+        );
     }
 }
