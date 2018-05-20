a69e7c0c30cd16f9848ac011d9ec46fa8c2e8d6e
alexandervmokhov <alexander.v.mokhov@gmail.com>
2018-05-20 08:45:39 +0300

    Синхронизация.

--- chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -1,18 +1,22 @@
 package ru.job4j.pseudo;
 
+import java.util.StringJoiner;
+
 /**
  * @author Alexander Mokhov (alexander.v.mokhov@gmail.com)
  * @version $Id$
  * @since 15.05.18
  */
 public class Triangle implements Shape {
+
     @Override
     public String draw() {
-        StringBuilder pic = new StringBuilder();
-        pic.append("   +   ");
-        pic.append("  + +  ");
-        pic.append(" + + + ");
-        pic.append("+ + + +");
-        return pic.toString();
+        StringJoiner pic = new StringJoiner(
+                System.lineSeparator(), "",
+                System.lineSeparator());
+                pic.add("  ^  ");
+                pic.add(" ^ ^ ");
+                pic.add("^^^^^");
+                return pic.toString();
     }
 }
--- chapter_002/src/test/java/ru/job4j/pseudo/TriangleTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/TriangleTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.pseudo;
 
 import org.junit.Test;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,16 +12,17 @@ import static org.junit.Assert.assertThat;
  */
 public class TriangleTest {
     @Test
-    public void whenDrawSquare() {
+    public void whenDrawTriangle() {
         Triangle triangle = new Triangle();
         assertThat(
                 triangle.draw(),
                 is(
-                        new StringBuilder()
-                                .append("   +   ")
-                                .append("  + +  ")
-                                .append(" + + + ")
-                                .append("+ + + +")
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
+                                .add("  ^  ")
+                                .add(" ^ ^ ")
+                                .add("^^^^^")
                                 .toString()
                 )
         );
